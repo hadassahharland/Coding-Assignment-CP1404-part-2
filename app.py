f2be6558330807edd0a31ff8795c100eb7536fcb
Dassa <hadassah.harland@gmail.com>
2015-11-03 10:02:57 +1000

    Pre-commit to major changes for input processing Introduced methods inside the app coding for todays_date

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ home_country = "Australia"
 todays_date = "2002/09/08"
 details = Details()
 details.add("Japan", "2002/09/07", "2002/09/09")
+# add to details inside app
+
 
 class BoxLayoutDemo(App):
     def build(self):
@@ -27,13 +29,16 @@ class BoxLayoutDemo(App):
         # If invalid country name return "Invalid Country: 'country_name'"
         # If invalid trip dates return "Invalid Dates: 'dates'"
         # On update_button_press if no change return "Refreshed 'time'"
-        # On update_button_press if change return "'from''($)' to 'to''($)'"
+        # On update_button_press if change return "'from id''($)' to 'to id''($)'"
         # If trip details changed return "Trip Details Accepted"
 
     def current_trip_location(self):
-        label = "Current trip Location: \n" + details.current_country(todays_date)
-        return label
+        return "Current trip Location: \n" + details.current_country(todays_date)
         # Centralize this label
 
+    def todays_date(self):
+        # retrieve today's date
+        return "Today is: \n" + todays_date
+        # Centralize this label
 
 BoxLayoutDemo().run()
